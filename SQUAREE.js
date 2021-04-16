e3d2324cf8418d1a47aa49cc73b60c42baad012b
vdrdz <78528752+vdrdz@users.noreply.github.com>
2021-04-15 22:21:13 -0700

    Add files via upload

---
+++ SQUAREE.js
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class SQuare extends Jframe{
+    public SQuare()
+    {
+        setTitle("SQUARE");
+        setSize(500,500);
+        setVisible(true);
+        setDefaultCloseOperation(Exit_ON_CLOSE);
+    }
+    public static void Square(String[] args)
+    {
+        SQuare square = new square();
+    }
+}
